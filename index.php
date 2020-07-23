61be078c13496c29b4d417d7f65858fb93f2631b
habonimana aime parfait <aphabona@gmail.com>
2020-07-23 18:28:27 +0200

    git add .

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//creation des variables
+
+$nom="La machine";
+$b=5;
+echo 'nom';
+
+//Opération arthimetique
+
+$d=5;
+$c=10;
+$somme =a+b;
+echo'somme';
+// fonction Booléen
+$too=true;
+$bar=false;
+
+//Création d'un tableau
+
+$legumes=array('carotte','poivron','aubergine','chou','chou_freur');
+?>
