2cd6c4b797ab3da41c8f9a8eb30059b07ea37fec
habonimana aime parfait <aphabona@gmail.com>
2020-07-23 18:32:51 +0200

    commit

--- index.php
+++ index.php
@@ -19,3 +19,25 @@ $bar=false;
 
 $legumes=array('carotte','poivron','aubergine','chou','chou_freur');
 ?>
+
+<?php
+//Utilisation de condition if et else
+
+$x=4;//affectation la valeur 4 à x
+$y=2;
+if($X>1){
+echo '$x contient une valeur superier à 1 <br>';
+}
+else{
+    echo '$x contient une valeur inferieur ou egal à 1 <br>';
+}
+if($x==$y){
+echo'$x et $y contient la meme valeur <br>';
+
+   }
+   else{
+
+   echo'$x et $y contiennent valeur <br>';
+
+   }
+   
